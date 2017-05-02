31cbd48bd62fb4278d29f9af2593fb4fefe4ee06
Charles Dunn <dunnc1551@gmail.com>
2017-05-02 17:41:42 -0600

    Add functionality to render link cards on page load and after a url has been submitted.

--- public/app.js
+++ public/app.js
@@ -5,12 +5,12 @@ $(() => {
 
   fetch('/api/urls')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => renderLinks(data))
 })
 
 $('.save-btn').on('click', function() {
   let $folderName = $('.folder-input').val();
-  fetch('api/folders', {
+  fetch('/api/folders', {
     'method': 'POST',
     'headers': {'Content-Type': 'application/json' },
     'body': JSON.stringify({ name: $folderName })
@@ -21,13 +21,44 @@ $('.save-btn').on('click', function() {
     }
     return response.json();
   })
-  .then(data => append(data.name))
+  .then(data => appendFolders(data.name))
+})
+
+$('.submit-btn').on('click', function(e) {
+  e.preventDefault();
+  let $url = $('.url-input').val();
+  fetch('/api/urls', {
+    'method': 'POST',
+    'headers': {'Content-Type': 'application/json'},
+    'body': JSON.stringify({ longUrl: $url, shortUrl: '', date: Date.now(), visits: 0, name: 'All' })
+  })
+  .then(response => {
+    if(!response.ok) {
+      console.log('error');
+    }
+    return response.json();
+  })
+  .then(data => appendLinks(data))
 })
 
 const renderFolders = (folders) => {
-  return folders.map(folder => append(folder.name));
+  return folders.map(folder => appendFolders(folder.name));
 }
 
-const append = (folderName) => {
+const appendFolders = (folderName) => {
   $('.folders').append(`<p>${folderName}</p>`);
+}
+
+const renderLinks = (links) => {
+  return links.map(linkCard => appendLinks(linkCard))
+}
+
+const appendLinks = (linkCard) => {
+  $('.link-list').append(`
+    <article>
+      <a href="${linkCard.longUrl}">${linkCard.longUrl}</a>
+      <a href="${linkCard.shortUrl}">${linkCard.shortUrl}</a>
+      <p>${linkCard.visits}</p>
+      <p>Date: ${linkCard.date}</p>
+    </article>`)
 }
