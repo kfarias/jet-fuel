2bfdd654f9fb068d4d9fb9c1315e8121e7a9eca2
Charles Dunn <dunnc1551@gmail.com>
2017-05-03 16:11:36 -0600

    Add db directory with initial migration.

---
+++ db/migrations/20170503155539_initial.js
@@ -0,0 +1,31 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('folders', (table) => {
+      table.increments('id').primary();
+      table.string('title');
+
+      table.timestamps();
+    }),
+
+    knex.schema.createTable('links', (table) => {
+      table.increments('id').primary();
+      table.string('longUrl');
+      table.string('shortUrl');
+      table.integer('visits');
+      table.datetime('date');
+      table.integer('folder_id').unsigned();
+      table.foreign('folder_id')
+        .references('folders.id');
+
+      table.timestamps();
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('folders'),
+    knex.schema.dropTable('links'),
+  ])
+};
