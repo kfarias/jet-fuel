aec495af51a7c21f8c2de6cd3b305eebbc0d173d
Charles Dunn <dunnc1551@gmail.com>
2017-05-05 10:20:59 -0600

    Add Procfile and production environment to knexfile.

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,17 @@ module.exports = {
       directory: './seeds'
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
   test: {
     client: 'pg',
     connection: 'postgres://localhost/jetfueltest',
